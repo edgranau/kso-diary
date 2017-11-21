008199e79e1af61546bd676c62274bdde14c00c8
Edwin Granados <edgranau@gmail.com>
2017-11-21 16:16:59 +1100

    use font-awesome icons

--- client/src/components/Layout.vue
+++ client/src/components/Layout.vue
@@ -1,21 +1,21 @@
 <template>
-  <q-layout ref='layout' view='hHh LpR fFf' :reveal='true'>
+  <q-layout ref='layout' view='hHh LpR fFf' :reveal='false'>
     <q-toolbar slot='header'>
       <q-btn flat @click="go('home')">
-        <q-icon name="home" />
+        <q-icon name="fa-home" />
       </q-btn>
       <q-toolbar-title>
         Epilepsy KSO Diary
       </q-toolbar-title>
       <q-btn flat @click="go('user')">
-        <q-icon name="person" />
+        <q-icon name="fa-user" />
       </q-btn>
     </q-toolbar>
     <router-view />
-    <q-fab class="fixed" color="secondary" style="bottom: 68px; right: 18px" icon="add circle outline" direction="left">
-      <q-fab-action color="primary" icon="touch app" @click="go('keto')"/>
-      <q-fab-action color="primary" icon="accessibility" @click="go('seizures')"/>
-      <q-fab-action color="primary" icon="note" @click="go('observations')"/>
+    <q-fab class="fixed" color="secondary" style="bottom: 48px; right: 18px" icon="fa-plus" direction="left">
+      <q-fab-action color="primary" icon="fa-user-md" @click="go('keto')"/>
+      <q-fab-action color="primary" icon="fa-bolt" @click="go('seizures')"/>
+      <q-fab-action color="primary" icon="fa-clipboard" @click="go('observations')"/>
     </q-fab>
     <q-toolbar slot='footer'>
       <q-toolbar-title>
@@ -59,6 +59,7 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="stylus">
+.q-toolbar
+  min-height: 0px
 </style>
