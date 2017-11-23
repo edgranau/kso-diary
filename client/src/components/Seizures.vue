68eedf7b5e3c61f186e9d77a517840481acda086
Edwin Granados <edgranau@gmail.com>
2017-11-24 08:44:15 +1100

    capture seizure details

--- client/src/components/Seizures.vue
+++ client/src/components/Seizures.vue
@@ -1,14 +1,107 @@
 <template>
-  <div>SEIZURES</div>
+  <div class="layout-padding card-list row items-start">
+    <q-card>
+      <q-card-title>
+        Add new seizure activity
+      </q-card-title>
+      <!-- <q-card-separator /> -->
+      <q-card-main>
+        <q-field label="Type">
+          <q-input v-model="type" type="text"/>
+        </q-field>
+        <q-field label="Date">
+          <q-datetime type="datetime" v-model="dateTime" />
+        </q-field>
+        <q-field label="Duration">
+          <q-input v-model="duration" type="text"/>
+        </q-field>
+        <q-field label="Cluster">
+          <q-radio v-model="cluster" val="yes" left-label label="Yes" />
+          <q-radio v-model="cluster" val="no" left-label label="No" />
+        </q-field>
+        <q-field label="Medication">
+          <q-radio v-model="medication" val="yes" left-label label="Yes" />
+          <q-radio v-model="medication" val="no" left-label label="No" />
+        </q-field>
+        <q-field label="Notes">
+          <q-input v-model="notes" type="text"/>
+        </q-field>
+      </q-card-main>
+      <!-- <q-card-separator /> -->
+      <q-card-actions align="end">
+        <q-btn color="primary" @click="submit">Submit</q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
 </template>
 
 <script>
+import { required } from 'vuelidate/lib/validators'
+import {
+  QBtn,
+  QCard,
+  QCardActions,
+  QCardMain,
+  QCardSeparator,
+  QCardTitle,
+  QDatetime,
+  QField,
+  QIcon,
+  QInput,
+  QOptionGroup,
+  QRadio,
+  Toast
+} from 'quasar'
 export default {
+  components: {
+    QBtn,
+    QCard,
+    QCardActions,
+    QCardMain,
+    QCardSeparator,
+    QCardTitle,
+    QDatetime,
+    QField,
+    QIcon,
+    QInput,
+    QOptionGroup,
+    QRadio,
+    Toast
+  },
   data () {
-    return {}
+    return {
+      type: undefined,
+      duration: undefined,
+      dateTime: new Date(),
+      cluster: undefined,
+      medication: undefined,
+      notes: undefined
+    }
+  },
+  validations: {
+    type: required,
+    duration: required,
+    dateTime: required,
+    cluster: required,
+    medication: required
+  },
+  methods: {
+    submit () {
+      Toast.create('Please review fields again.')
+    },
+    submitRatio () {
+      Toast.create('Please review ratio fields again.')
+    }
   }
 }
 </script>
 
-<style>
+<style lang='stylus'>
+.card-list
+  .q-card
+    width 400px
+  @media (max-width $breakpoint-xs-max)
+    .q-card
+      width 100%
+
 </style>
