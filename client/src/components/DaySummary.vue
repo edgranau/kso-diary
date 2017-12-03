bac129f50589e833c626fd144ed509ed2788b7d1
Edwin Granados <edgranau@gmail.com>
2017-12-03 23:03:09 +1100

    display full day's details using modal

--- client/src/components/DaySummary.vue
+++ client/src/components/DaySummary.vue
@@ -1,63 +1,147 @@
 <template>
   <div class="layout-padding card-list row items-start">
-    <!-- style="width: 210px" -->
     <q-card inline v-for="(value, key) in payload" :key="key">
       <q-card-title>
-        <span slot="subtitle">{{ key }}</span>
+        <span>{{ key }}</span>
       </q-card-title>
       <q-card-separator />
       <q-card-main>
-        <div class="row">
-          <div class="col-12">
+        <q-list>
+          <q-item>
+            <q-item-side>
+              <q-item-tile color="yellow" icon="fa-sun-o" />
+            </q-item-side>
+            <q-item-main>
+              <q-item-tile label>{{ payload[key]['keto']['am']['value'] }}</q-item-tile>
+            </q-item-main>
+          </q-item>
+          <!-- <q-collapsible icon="fa-bolt" label="Seizures">
+            <q-item>
+              <q-item-side>
+                <q-item-tile color="secondary" icon="fa-bolt" />
+              </q-item-side>
+              <q-item-main>
+                <q-item-tile label>Absence</q-item-tile>
+                <q-item-tile sublabel>5sec, no cluster, no medication</q-item-tile>
+                <q-item-tile sublabel>no notes</q-item-tile>
+              </q-item-main>
+            </q-item>
+            <q-item>
+              <q-item-side>
+                <q-item-tile color="secondary" icon="fa-bolt" />
+              </q-item-side>
+              <q-item-main>
+                <q-item-tile label>Absence</q-item-tile>
+                <q-item-tile sublabel>5sec, no cluster, no medication</q-item-tile>
+                <q-item-tile sublabel>no notes</q-item-tile>
+              </q-item-main>
+            </q-item>
+          </q-collapsible> -->
+          <q-item link @click="openModal(key)">
+            <q-item-main inset>
+              <q-chip v-if="hasSeizures(key)" icon="fa-bolt" color="secondary">
+                {{ payload[key]['seizures'].length }}
+              </q-chip>
+              <q-chip v-if="hasObservations(key)" icon="fa-comment" color="tertiary">
+                {{ payload[key]['observations'].length }}
+              </q-chip>
+            </q-item-main>
+          </q-item>
+          <q-item>
+            <q-item-side>
+              <q-item-tile color="indigo" icon="fa-moon-o" />
+            </q-item-side>
+            <q-item-main>
+              <q-item-tile label>{{ payload[key]['keto']['pm']['value'] }}</q-item-tile>
+            </q-item-main>
+          </q-item>
+        </q-list>
+      </q-card-main>
+    </q-card>
+    <q-modal ref="DayDetailModal" >
+      <div class="layout-padding card-list row items-start">
+        <q-card>
+          <q-card-title>
+            <span>{{ modalData.date }}</span>
+          </q-card-title>
+          <q-card-separator />
+          <q-card-main>
             <q-list>
               <q-item>
                 <q-item-side>
                   <q-item-tile color="yellow" icon="fa-sun-o" />
                 </q-item-side>
                 <q-item-main>
-                  <q-item-tile label>{{ payload[key]['keto']['am']['value'] }}</q-item-tile>
+                  <q-item-tile label>{{ modalData.amValue }}</q-item-tile>
                 </q-item-main>
               </q-item>
-              <q-item v-if="hasObservations(key) || hasSeizures(key)">
-                <q-item-side/>
-                <q-item-main>
-                  <q-chip v-if="hasSeizures(key)" icon="fa-bolt" color="secondary">
-                    {{ payload[key]['seizures'].length }}
-                  </q-chip>
-                  <q-chip v-if="hasObservations(key)" icon="fa-comment" color="tertiary">
-                    {{ payload[key]['observations'].length }}
-                  </q-chip>
+              <q-item-separator inset />
+              <q-list-header>
+                Seizures
+              </q-list-header>
+              <q-item v-for="(seizure, key) in modalData.seizures" :key="key">
+                <q-item-side>
+                  <q-item-tile color="secondary" icon="fa-bolt" />
+                </q-item-side>
+                <q-item-main sublabel-lines=5>
+                  <q-item-tile label>{{ seizure.type }} @ {{ seizure.time }}</q-item-tile>
+                  <q-item-tile sublabel>{{ seizureDetails(seizure) }}</q-item-tile>
+                  <q-item-tile sublabel>{{ seizure.notes }}</q-item-tile>
                 </q-item-main>
               </q-item>
+              <q-item-separator inset />
+              <q-list-header>
+                Observations
+              </q-list-header>
+              <q-item v-for="(observation, key) in modalData.observations" :key="key">
+                <q-item-side>
+                  <q-item-tile color="tertiary" icon="fa-comment" />
+                </q-item-side>
+                <q-item-main sublabel-lines=5>
+                  <q-item-tile label>{{ observation.title }} @ {{ observation.time }}</q-item-tile>
+                  <q-item-tile sublabel>{{ observation.details }}</q-item-tile>
+                </q-item-main>
+              </q-item>
+              <q-item-separator inset />
               <q-item>
                 <q-item-side>
                   <q-item-tile color="indigo" icon="fa-moon-o" />
                 </q-item-side>
                 <q-item-main>
-                  <q-item-tile label>{{ payload[key]['keto']['pm']['value'] }}</q-item-tile>
+                  <q-item-tile label>{{ modalData.pmValue }}</q-item-tile>
                 </q-item-main>
               </q-item>
             </q-list>
-          </div>
-        </div>
-      </q-card-main>
-    </q-card>
+          </q-card-main>
+          <q-card-actions>
+            <q-btn color="primary" @click="closeModal()">Close</q-btn>
+          </q-card-actions>
+        </q-card>
+      </div>
+    </q-modal>
   </div>
 </template>
 
 <script>
 import { mapActions, mapState } from 'vuex'
 import {
+  QBtn,
   QCard,
+  QCardActions,
   QCardMain,
   QCardSeparator,
   QCardTitle,
   QChip,
+  QCollapsible,
   QList,
+  QListHeader,
   QItem,
+  QItemSeparator,
   QItemSide,
   QItemTile,
-  QItemMain
+  QItemMain,
+  QModal,
+  QModalLayout
 } from 'quasar'
 export default {
   name: 'daySummary',
@@ -67,19 +151,33 @@ export default {
     pmValue: String
   },
   components: {
+    QBtn,
     QCard,
+    QCardActions,
     QCardMain,
     QCardSeparator,
     QCardTitle,
     QChip,
+    QCollapsible,
     QList,
+    QListHeader,
     QItem,
+    QItemSeparator,
     QItemSide,
     QItemTile,
-    QItemMain
+    QItemMain,
+    QModal,
+    QModalLayout
   },
   data () {
     return {
+      modalData: {
+        date: '',
+        amValue: 0,
+        pmValue: 0,
+        seizures: [],
+        observations: []
+      }
     }
   },
   computed: {
@@ -98,6 +196,25 @@ export default {
     },
     hasSeizures (key) {
       return (this.payload[key]['seizures'].length > 0)
+    },
+    openModal (key) {
+      this.modalData.date = key
+      this.modalData.amValue = this.payload[key]['keto']['am']['value']
+      this.modalData.pmValue = this.payload[key]['keto']['pm']['value']
+      this.modalData.seizures = this.payload[key]['seizures']
+      this.modalData.observations = this.payload[key]['observations']
+      this.$refs.DayDetailModal.open()
+    },
+    closeModal () {
+      this.modalData.date = ''
+      this.modalData.amValue = 0
+      this.modalData.pmValue = 0
+      this.modalData.seizures = []
+      this.modalData.observations = []
+      this.$refs.DayDetailModal.close()
+    },
+    seizureDetails (seizure) {
+      return `${seizure.duration}, ${seizure.cluster}, ${seizure.medication}`
     }
   }
 }
@@ -108,6 +225,8 @@ export default {
 .card-list
   .q-card
     width 400px
+    .q-card-main
+      min-height 178px
   @media (max-width $breakpoint-xs-max)
     .q-card
       width 100%
